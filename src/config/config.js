345a59ab6094a1436bce01bbe7a9a0db4983d2d8
erreboost <serviceserre@gmail.com>
2024-10-28 15:59:32 +0000

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://lrb-app2.grupoerre.pt/";
+export const API_BASE_URL = "https://lrb-app2.grupoerre.pt";
 export const API_ENDPOINT = `${API_BASE_URL}/api/tree/get-trees`;
